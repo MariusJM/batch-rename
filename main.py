b2776ddb61114106391baaff48867ea83393c275
MariusJM <mariusjm.code@gmail.com>
2024-01-16 13:36:38 +0200

    fix Ui autoscaling

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ class App:
         background = "#e0e0e0"
 
         # top frame for file selection
-        self.frame_select = tk.Frame(master, width=600, height=40, bg=background)
-        self.frame_select.pack_propagate(False)
+        self.frame_select = tk.Frame(master, bg=background)
+        self.frame_select.pack_propagate(True)
 
         # browse button
-        self.button_browse = tk.Button(master=self.frame_select, width=200, height=40, text="Browse")
+        self.button_browse = tk.Button(master=self.frame_select, text="Browse")
         
-        self.button_browse.pack(anchor="n", padx=5, pady=5)
+        self.button_browse.pack(side="left", padx=5,pady=5, fill="both", expand=True)
 
         # rename options frame
         self.frame_rename = tk.Frame(master=master, width=600, height=65, bg=background)
@@ -27,28 +27,28 @@ class App:
         
         # prefix
         self.frame_prefix = tk.Frame(master=self.frame_rename, width=190, height=30, bg=background)
-        self.label_prefix = tk.Label(master=self.frame_prefix, text="Prefix:")
+        self.label_prefix = tk.Label(master=self.frame_prefix, text="Prefix:", bg=background)
         self.entry_prefix = tk.Entry(master=self.frame_prefix, justify="left")
 
-        self.frame_prefix.pack(side="left", padx=5, pady=5)
+        self.frame_prefix.pack(fill="x", expand=True, side="left", padx=5, pady=5)
         self.label_prefix.pack(side="left", padx=5, pady=5, anchor="w")
         self.entry_prefix.pack(fill="x", expand=True)
 
         # name
         self.frame_name = tk.Frame(master=self.frame_rename, width=190, height=30, bg=background)
-        self.label_name = tk.Label(master=self.frame_name, text="Name:")
+        self.label_name = tk.Label(master=self.frame_name, text="Name:", bg=background)
         self.entry_name = tk.Entry(master=self.frame_name, justify="left")
 
-        self.frame_name.pack(side="left", padx=5, pady=5)
+        self.frame_name.pack(fill="x", expand=True, side="left", padx=5, pady=5)
         self.label_name.pack(side="left", padx=5, pady=5, anchor="w")
         self.entry_name.pack(fill="x", expand=True)
         
         # suffix
         self.frame_suffix = tk.Frame(master=self.frame_rename, width=190, height=30, bg=background)
-        self.label_suffix = tk.Label(master=self.frame_suffix, text="Suffix:")
+        self.label_suffix = tk.Label(master=self.frame_suffix, text="Suffix:", bg=background)
         self.entry_suffix = tk.Entry(master=self.frame_suffix, justify="left")
 
-        self.frame_suffix.pack(side="left", padx=5, pady=5)
+        self.frame_suffix.pack(fill="x", expand=True, side="left", padx=5, pady=5)
         self.label_suffix.pack(side="left", padx=5, pady=5, anchor="w")
         self.entry_suffix.pack(fill="x", expand=True)
 
@@ -66,10 +66,11 @@ class App:
 
 
         # preview window
-        self.frame_preview = tk.Frame(master=master, width=600, height=60, bg="#e0e0e0")
-        self.label_original_name_label = tk.Label(master=self.frame_preview, text="Original file name")
+        self.frame_preview = tk.Frame(master=master, bg="#e0e0e0")
+        self.frame_preview.propagate(True)
+        self.label_original_name_label = tk.Label(master=self.frame_preview, text="Original file name", bg=background)
         self.label_original_name = tk.Label(master=self.frame_preview, text="file.png")
-        self.label_new_name_label = tk.Label(master=self.frame_preview, text="New file name")
+        self.label_new_name_label = tk.Label(master=self.frame_preview, text="New file name", bg=background)
         self.label_new_name = tk.Label(master=self.frame_preview, text="prefix_file_siffix.png")
 
         self.label_original_name_label.pack(padx=5,pady=5, anchor="w")
@@ -78,11 +79,11 @@ class App:
         self.label_new_name.pack(padx=5,pady=5, anchor="w")
 
         #pack frames
-        self.frame_select.pack(expand=False, padx=5,pady=5, anchor="w")
-        self.frame_rename.pack(expand=False, padx=5,pady=5, anchor="w")
-        self.frame_execute.pack(expand=False, padx=5,pady=5, anchor="w")
-        self.frame_preview.pack(expand=False, padx=5,pady=5, anchor="w")
-        master.resizable(False, False)
+        self.frame_select.pack(fill="x", expand=True, padx=5,pady=5, anchor="w")
+        self.frame_rename.pack(fill="x", expand=True, padx=5,pady=5, anchor="w")
+        self.frame_execute.pack(fill="x", expand=True, padx=5,pady=5, anchor="w")
+        self.frame_preview.pack(fill="x", expand=True, padx=5,pady=5, anchor="w")
+        master.resizable(True, False)
 
 
 
